5e4feefec33ecbce46909c8d86ddb945fde98012
Robert Haines <rhaines@manchester.ac.uk>
2021-12-05 19:27:50 +0000

    Refactor day 5 to pre-compute the coordinate spans.

--- lib/aoc2021/days/hydrothermal_venture.rb
+++ lib/aoc2021/days/hydrothermal_venture.rb
@@ -23,29 +23,13 @@ module AOC2021
     end
 
     def map_overlaps(list, aligned_only: true)
-      grid = {}
+      grid = Hash.new(0)
 
-      list.each do |(x1, y1), (x2, y2)|
-        x_range = x1 > x2 ? x1.downto(x2).to_a : x1.upto(x2).to_a
-        y_range = y1 > y2 ? y1.downto(y2).to_a : y1.upto(y2).to_a
+      list.each do |aligned, coords|
+        next if aligned_only && !aligned
 
-        if x1 == x2
-          y_range.each do |y|
-            grid[[x1, y]] ||= 0
-            grid[[x1, y]] += 1
-          end
-        elsif y1 == y2
-          x_range.each do |x|
-            grid[[x, y1]] ||= 0
-            grid[[x, y1]] += 1
-          end
-        else
-          unless aligned_only
-            x_range.zip(y_range).each do |x, y|
-              grid[[x, y]] ||= 0
-              grid[[x, y]] += 1
-            end
-          end
+        coords.each do |x, y|
+          grid[[x, y]] += 1
         end
       end
 
@@ -53,9 +37,22 @@ module AOC2021
     end
 
     def read_coords(input)
-      input.map do |line|
+      pairs = input.map do |line|
         line.split(' -> ').map do |coords|
           coords.split(',').map(&:to_i)
+        end
+      end
+
+      pairs.map do |(x1, y1), (x2, y2)|
+        x_range = x1 > x2 ? x1.downto(x2).to_a : x1.upto(x2).to_a
+        y_range = y1 > y2 ? y1.downto(y2).to_a : y1.upto(y2).to_a
+
+        if x1 == x2
+          [true, ([x1] * y_range.length).zip(y_range)]
+        elsif y1 == y2
+          [true, x_range.zip([y1] * x_range.length)]
+        else
+          [false, x_range.zip(y_range)]
         end
       end
     end
