a08e93abef1f300ee7103a57097aff7a0ad7e468
Robert Haines <rhaines@manchester.ac.uk>
2021-12-10 17:41:14 +0000

    Refactor day 9 to use a `Map` class.

--- lib/aoc2021/days/smoke_basin.rb
+++ lib/aoc2021/days/smoke_basin.rb
@@ -10,71 +10,76 @@ require 'aoc2021'
 
 module AOC2021
   class SmokeBasin < Day
-    def setup
-      @map = read_input_file.split("\n").map do |line|
-        line.chars.map(&:to_i)
-      end
-
-      @low_points = find_low_points(@map)
+    def setup(input = read_input_file)
+      @map = Map.new(input)
     end
 
     def part1
-      risk_level
+      @map.low_points.values.sum + @map.low_points.length
     end
 
     def part2
-      basin_sizes.max(3).reduce(&:*)
+      @map.basin_sizes.max(3).reduce(&:*)
     end
 
-    def risk_level(low_points = @low_points)
-      low_points.values.sum + low_points.length
-    end
+    class Map
+      attr_reader :low_points
 
-    def basin_sizes(map = @map, low_points = @low_points)
-      low_points.keys.map do |point|
-        explore_basin(map, point).length
+      def initialize(input)
+        @map = input.split("\n").map do |line|
+          line.chars.map(&:to_i)
+        end
+
+        @max_y = @map.length - 1
+        @max_x = @map[0].length - 1
+
+        @low_points = find_low_points
       end
-    end
 
-    def explore_basin(map, start, basin = [])
-      return basin if basin.include?(start)
-
-      basin << start
-
-      max_y = map.length - 1
-      max_x = map[0].length - 1
-      neighbours(start[1], start[0], max_x, max_y).map do |nx, ny|
-        next if map[ny][nx] == 9
-
-        explore_basin(map, [ny, nx], basin)
-      end.compact.flatten(1).uniq
-    end
-
-    def find_low_points(input)
-      max_y = input.length - 1
-      max_x = input[0].length - 1
-      low_points = {}
-
-      input.each_with_index do |line, y|
-        line.each_with_index do |point, x|
-          low = neighbours(x, y, max_x, max_y).each do |nx, ny|
-            break false if input[ny][nx] <= point
-          end
-
-          low_points[[y, x]] = point if low
+      def basin_sizes
+        @low_points.keys.map do |point|
+          explore_basin(point).length
         end
       end
 
-      low_points
-    end
+      def explore_basin(start, basin = [])
+        return basin if basin.include?(start)
 
-    def neighbours(x, y, max_x, max_y)
-      [
-        y.zero? ? nil : [x, y - 1],
-        y == max_y ? nil : [x, y + 1],
-        x.zero? ? nil : [x - 1, y],
-        x == max_x ? nil : [x + 1, y]
-      ].compact
+        basin << start
+
+        neighbours_for(start[1], start[0]).map do |nx, ny|
+          next if @map[ny][nx] == 9
+
+          explore_basin([ny, nx], basin)
+        end.compact.flatten(1).uniq
+      end
+
+      private
+
+      def find_low_points
+        low_points = {}
+
+        @map.each_with_index do |line, y|
+          line.each_with_index do |point, x|
+            low = neighbours_for(x, y).each do |nx, ny|
+              break false if @map[ny][nx] <= point
+            end
+
+            low_points[[y, x]] = point if low
+          end
+        end
+
+        low_points
+      end
+
+      def neighbours_for(x, y)
+        [
+          y.zero? ? nil : [x, y - 1],
+          y == @max_y ? nil : [x, y + 1],
+          x.zero? ? nil : [x - 1, y],
+          x == @max_x ? nil : [x + 1, y]
+        ].compact
+      end
     end
   end
 end
--- test/days/smoke_basin_test.rb
+++ test/days/smoke_basin_test.rb
@@ -10,7 +10,7 @@ require 'test_helper'
 require 'aoc2021/days/smoke_basin'
 
 class AOC2021::SmokeBasinTest < MiniTest::Test
-  MAP = <<~EOM.split("\n").map { |line| line.chars.map(&:to_i) }
+  MAP = <<~EOM
     2199943210
     3987894921
     9856789892
@@ -20,36 +20,40 @@ class AOC2021::SmokeBasinTest < MiniTest::Test
 
   def setup
     @sb = AOC2021::SmokeBasin.new
+    @sb.setup(MAP)
+    @map = AOC2021::SmokeBasin::Map.new(MAP)
   end
 
-  def test_find_low_points
+  def test_build_map
     assert_equal(
       { [0, 1] => 1, [0, 9] => 0, [2, 2] => 5, [4, 6] => 5 },
-      @sb.find_low_points(MAP)
+      @map.low_points
     )
   end
 
-  def test_risk_level
-    assert_equal(15, @sb.risk_level(@sb.find_low_points(MAP)))
+  def test_part1
+    assert_equal(15, @sb.part1)
   end
 
   def test_explore_basin
     assert_equal(
       [[0, 1], [0, 0], [1, 0]],
-      @sb.explore_basin(MAP, [0, 1])
+      @map.explore_basin([0, 1])
     )
 
     assert_equal(
       [
         [4, 6], [3, 6], [3, 7], [2, 7], [4, 7], [4, 8], [3, 8], [4, 9], [4, 5]
       ],
-      @sb.explore_basin(MAP, [4, 6])
+      @map.explore_basin([4, 6])
     )
   end
 
   def test_basin_sizes
-    assert_equal(
-      [3, 9, 14, 9], @sb.basin_sizes(MAP, @sb.find_low_points(MAP))
-    )
+    assert_equal([3, 9, 14, 9], @map.basin_sizes)
+  end
+
+  def test_part2
+    assert_equal(1134, @sb.part2)
   end
 end
