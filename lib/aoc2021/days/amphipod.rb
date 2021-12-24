ffb0c5aa2f5cb0567444d088c6b07e8a148d5507
Robert Haines <rhaines@manchester.ac.uk>
2021-12-24 08:53:57 +0000

    Solution for day 23, part 2.

--- lib/aoc2021/days/amphipod.rb
+++ lib/aoc2021/days/amphipod.rb
@@ -14,5 +14,88 @@ module AOC2021
     def part1
       11_332
     end
+
+    # Worked out by hand.
+    def part2
+      49_936
+    end
   end
 end
+
+# rubocop:disable Layout/LeadingCommentSpace, Layout/CommentIndentation
+
+#############
+#...........#
+###A#C#B#D###
+  #D#C#B#A#
+  #D#B#A#C#
+  #B#A#D#C#
+  #########
+
+#############
+#A....B.C.C.#  1148
+###A#.#B#D###
+  #D#.#B#A#
+  #D#.#A#C#
+  #B#.#D#C#
+  #########
+
+#############
+#A......C.C.#  170
+###A#.#.#D###
+  #D#B#.#A#
+  #D#B#A#C#
+  #B#B#D#C#
+  #########
+
+#############
+#AA.D...C.C.#  7008
+###A#.#.#D###
+  #D#B#.#A#
+  #D#B#.#C#
+  #B#B#.#C#
+  #########
+
+#############
+#AA.D.......#  1100
+###A#.#.#D###
+  #D#B#.#A#
+  #D#B#C#C#
+  #B#B#C#C#
+  #########
+
+#############
+#AA.D.D....A#  5404
+###A#.#C#.###
+  #D#B#C#.#
+  #D#B#C#.#
+  #B#B#C#.#
+  #########
+
+#############
+#AA........A#  15000
+###A#.#C#.###
+  #D#B#C#.#
+  #D#B#C#D#
+  #B#B#C#D#
+  #########
+
+#############
+#AA.......AA#  20078
+###.#B#C#D###
+  #.#B#C#D#
+  #.#B#C#D#
+  #.#B#C#D#
+  #########
+
+#############
+#...........#  28
+###A#B#C#D###
+  #A#B#C#D#
+  #A#B#C#D#
+  #A#B#C#D#
+  #########
+
+#              49936
+
+# rubocop:enable Layout/LeadingCommentSpace, Layout/CommentIndentation
